f03b341fd0dd42696d707d77694ce9f7ad3f51ee
Swati P <67189695+swati-padmanabhan@users.noreply.github.com>
2024-09-06 17:30:49 +0530

    initial commit

---
+++ Controllers/SessionController.cs
@@ -0,0 +1,41 @@
+﻿using System.Web.Mvc;
+
+namespace SessionAndSessionTimeout.Controllers
+{
+    public class SessionController : Controller
+    {
+        // GET: Session
+        public ActionResult SetSession()
+        {
+            //set session data
+            Session["Message"] = "Hello, Jerry!";
+            return RedirectToAction("GetSession");
+        }
+
+        //this action retrieves the session variable and displays a message indicating whether it was found
+        public ActionResult GetSession()
+        {
+            var message = Session["Message"];
+
+            if (message != null)
+            {
+                //set a TempData message indicating the session variable was found
+                TempData["Status"] = $"Session Variable Found: {message}";
+            }
+            else
+            {
+                TempData["Status"] = "No session variable found.";
+            }
+            return View();
+        }
+
+        public ActionResult ClearSession()
+        {
+            //remove the session variable "Message" by setting it to null
+            Session["Message"] = null;
+
+            return RedirectToAction("GetSession");
+        }
+
+    }
+}
